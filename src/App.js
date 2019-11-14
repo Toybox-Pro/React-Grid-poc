e489b33dfe75299232b8365238fdaab691046952
mnameit1 <mnameit1@iMac.local>
2019-11-14 14:13:25 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Home } from './components/Home Component/Home.component';
 import { Cases } from './components/Cases Component/Cases.component';
 import Logo from './logo.png';
 import GridContext from './context';
+// import { OAuth2 } from "fetch-mw-oauth2";
 
 class App extends Component {
 
@@ -33,6 +34,52 @@ class App extends Component {
       departureDate
     })
   }
+
+  // componentDidMount() {
+  //   const auth = new OAuth2({
+
+  //   });
+
+  //   const response = await auth.fetch('https://poc-pov-playground-dev-ed.my.salesforce.com/services/apexrest/getCases/851308', {
+  //     method: 'GET',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     }
+  //   })
+  //   console.log(response)
+  //   const obj = {
+  //     grantType: 'password',
+  //     clientId: '3MVG9G9pzCUSkzZu0iNkVK0Xdxr9mGsfs7sNZbVaEf3uh0AByvloQSMBebzzX32rZpuESJ_SeU2.JvVFOS_f.',
+  //     clientSecret: 'D0F6FDE6C71A18895DFEF232AD87143A353AA73978C65F3A0CBB7AC16405771B',
+  //     userName: 'ayan.hore@pocplayground.com',
+  //     password: 'ayan@1986@horeqUR3SwNrqGOFdc9k80r7ki3j',
+  //     tokenEndPoint: 'https://poc-pov-playground-dev-ed.my.salesforce.com/services/oauth2/token'
+  //   };
+  //   fetch('https://poc-pov-playground-dev-ed.my.salesforce.com', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Accept': 'application/json',
+  //       'Content-Type': 'application/json',
+  //       'Access-Control-Allow-Origin': '*'
+  //     },
+  //     body: JSON.stringify(obj)
+  //   })
+  //     .then((res) => console.log(res))
+  //   fetch('https://poc-pov-playground-dev-ed.my.salesforce.com/services/oauth2/token',
+
+  //     {
+
+  //       method: 'post',
+
+  //       mode: 'no-cors',
+
+  //       body: `grant_type=password&client_id='3MVG9G9pzCUSkzZu0iNkVK0Xdxr9mGsfs7sNZbVaEf3uh0AByvloQSMBebzzX32rZpuESJ_SeU2.JvVFOS_f.'&client_secret='D0F6FDE6C71A18895DFEF232AD87143A353AA73978C65F3A0CBB7AC16405771B'&username=ayan.hore@pocplayground.com'+'&password=ayan@1986@hore'+'qUR3SwNrqGOFdc9k80r7ki3j`
+
+  //     })
+
+  //     .then((res) => JSON.parse(res.json()))
+  //     .then(data => console.log(data))
+  // }
   render() {
 
     return (
